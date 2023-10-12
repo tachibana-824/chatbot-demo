2529198a586f1ab44183ac4006594812adee860f
tachibana-824 <132441668+tachibana-824@users.noreply.github.com>
2023-10-12 17:57:21 +0900

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD0HaTIQEbFOt8on4W8GY8hhivXvyw8I0w",
-    authDomain: "chatbot-demo-4670b.firebaseapp.com",
-    projectId: "chatbot-demo-4670b",
-    storageBucket: "chatbot-demo-4670b.appspot.com",
-    messagingSenderId: "618800922361",
-    appId: "1:618800922361:web:5dfbefeec1f083b9bfec28",
-    measurementId: "G-3N87WLF5YR"
+
   };
 
 export default firebaseConfig
